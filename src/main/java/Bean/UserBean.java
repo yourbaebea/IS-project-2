45ede2441213bf15762c4e5768cc9648a5d315d2
Ana Beatriz Marques <anafm@student.dei.uc.pt>
2021-10-23 20:25:59 +0100

    final update

---
+++ src/main/java/Bean/UserBean.java
@@ -0,0 +1,42 @@
+package Bean;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import Classes.*;
+
+/**
+ * Session Bean implementation class Bean
+ */
+//@Stateless
+//@LocalBean
+public class UserBean implements UserBeanRemote {
+
+	
+    public UserBean() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void RegisterUser(User user) {
+		//em.persist(user)
+    	
+    }
+
+
+	/*
+    
+    public void example(String name) {
+    	String jpqlU = "SELECT DISTINCT r FROM Users r WHERE r.name = :name";
+		
+		TypedQuery<User> typedQueryU = em.createQuery(jpqlU, User.class);
+		typedQueryU.setParameter("name", name);
+		User mylistU = typedQueryU.getSingleResult();
+    	
+		do stuff.........
+    	em.persist(.....);  	
+    }
+	*/
+
+}
+   
